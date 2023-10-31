5ce289cf2d5044e89e66d348ef1c950ed2d07bcc
moshe <moshehadar001@gmail.com>
2023-10-31 16:35:17 +0200

    moshe ha-meleh

--- MichaelBusso.js
+++ MichaelBusso.js
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 console.log("hello world");
 const name = 'Michael Busso';
 const lastName = 'Moshe Hadar';
+=======
+const name = 'Michael Busso';
+111111111111111111111111111111111111111111:
+>>>>>>> 8a52b2dbf6a62580d72721f3a8dceda6cb891a12
