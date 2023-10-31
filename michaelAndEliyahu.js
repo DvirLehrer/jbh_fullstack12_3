6fe4b78d43cd485cc5455bb268fc25ca005fbea7
MichaelBusso <mb0548984966@gmail.com>
2023-10-31 17:57:09 +0200

    Final project

--- michaelAndEliyahu.js
+++ michaelAndEliyahu.js
@@ -1,8 +1,15 @@
-let response="";
-setInterval(()=>{
-    if (response.length>0){
-        document.getElementById('display').style.display='block';
-    }
+let response = '';
 
-},100);
-document.getElementById('textBox').oninput = setTimeout(()=> response ="abc",500)
+setInterval(() => {
+    if (response.length > 0) {
+        document.getElementById('display').style.display = 'block';
+        document.getElementById('display').innerText = response;
+    }
+}, 100);
+
+document.getElementById('textBox').addEventListener('input', () => {
+    setTimeout(() => {
+        console.log('hi')
+        response = 'abc';
+    }, 500)
+}) 
