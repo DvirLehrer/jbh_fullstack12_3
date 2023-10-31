7ff499b5d4bbdb7db888336e76422069755975f6
Aharon Cohen <aharoncohen7@gmail.com>
2023-10-31 18:25:02 +0200

    done

--- ahrons.js
+++ ahrons.js
@@ -1,7 +1,21 @@
-function sort(){
+
+let response = "";
 
 
 
+let x= setInterval(() => {
+   
+    if(response.length>0){
+        // console.log("1234");
+        document.getElementById("results").innerHTML = response;
+        // clearInterval(x);
+    }
+}, 100);
 
-    
+function u(){
+    setTimeout(() => {
+        response=document.getElementById("input2").value;
+        
+    }, 500);
+
 }
