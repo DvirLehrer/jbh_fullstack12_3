92b69743dbd56be32b152117b67fe9a305721f08
MichaelBusso <mb0548984966@gmail.com>
2023-11-01 12:28:46 +0200

    Back to final 2

--- michaelAndEliyahu.js
+++ michaelAndEliyahu.js
@@ -4,12 +4,13 @@ setInterval(() => {
     if (response.length > 0) {
         document.getElementById('display').style.display = 'block';
         document.getElementById('display').innerText = response;
-    }
+    }else{document.getElementById('display').style.display = 'none';}
 }, 100);
 
 document.getElementById('textBox').addEventListener('input', () => {
     setTimeout(() => {
         console.log('hi')
+        // response = document.getElementById('textBox').value;
         response = 'abc';
     }, 500)
 }) 
